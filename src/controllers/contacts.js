1c8b7e436b0786631a1b2f9dcd08b919fb6bb643
Subx1s0o <nazarsabardin@gmail.com>
2025-01-05 00:40:30 +0200

    add mail

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -9,110 +9,120 @@ import { parseSortParams } from '../utils/parseSortParams.js';
 import { parseFilterParams } from '../utils/parseFilterParams.js';
 import createHttpError from 'http-errors';
 import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { saveFileToUploadDir } from '../utils/saveFileToUploadDir.js';
+import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
+import { env } from '../utils/env.js';
 
-// Отримання всіх контактів
 export const getContactsController = async (req, res, next) => {
+  const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+  const filter = parseFilterParams(req.query);
 
-    const { page, perPage } = parsePaginationParams(req.query);
-    const { sortBy, sortOrder } = parseSortParams(req.query);
-    const filter = parseFilterParams(req.query);
+  const contacts = await getAllContacts({
+    page,
+    perPage,
+    sortBy,
+    sortOrder,
+    filter,
+    userId: req.user._id,
+  });
 
-    const contacts = await getAllContacts({
-      page,
-      perPage,
-      sortBy,
-      sortOrder,
-      filter,
-      userId: req.user._id,
-    });
-
-    res.status(200).json({
-      status: 200,
-      message: 'Successfully found contacts!',
-      data: contacts,
-    });
-
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  });
 };
 
-// Отримання контакту за ID
 export const getContactByIdController = async (req, res, next) => {
   const { id } = req.params;
+  const contact = await getContactById(id, req.user._id);
 
-    const contact = await getContactById(id, req.user._id);
-
-    if (!contact) {
-      return next(createHttpError(404, 'Contact not found'));
-    }
-
-    res.status(200).json({
-      status: 200,
-      message: `Successfully found contact with id ${id}!`,
-      data: contact,
-    });
+  if (!contact) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found contact with id ${id}!`,
+    data: contact,
+  });
 };
 
+export const createContactController = async (req, res) => {
+  const photo = req.file;
+  let photoUrl;
 
-export const createContactController = async (req, res, next) => {
-
-    const contactSchema = {
-      name: req.body.name,
-      phoneNumber: req.body.phoneNumber,
-      email: req.body.email,
-      isFavourite: req.body.isFavourite,
-      contactType: req.body.contactType,
-      userId: req.user._id,
-    };
-
-    if (
-      !contactSchema.name ||
-      !contactSchema.phoneNumber ||
-      !contactSchema.contactType
-    ) {
-      return next(createHttpError(400, 'Missing required fields'));
+  if (photo) {
+    if (env('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
     }
-    
+  }
 
-    const contact = await createContact(contactSchema, req.user._id);
+  const contactSchema = {
+    name: req.body.name,
+    phoneNumber: req.body.phoneNumber,
+    email: req.body.email,
+    isFavourite: req.body.isFavourite,
+    contactType: req.body.contactType,
+    userId: req.user._id,
+    photo: photoUrl,
+  };
 
-    res.status(201).send({
-      status: 201,
-      message: 'Successfully created a contact!',
-      data: contact,
-    });
+  const contact = await createContact(contactSchema, req.user._id);
 
+  res.status(201).send({
+    status: 201,
+    message: 'Successfully created a contact!',
+    data: contact,
+  });
 };
 
-// Оновлення контакту
 export const patchContactController = async (req, res, next) => {
+  const { id } = req.params;
+  const photo = req.file;
+  let photoUrl;
 
+  if (photo) {
+    if (env('ENABLE_CLOUDINARY') === 'true') {
+      photoUrl = await saveFileToCloudinary(photo);
+    } else {
+      photoUrl = await saveFileToUploadDir(photo);
+    }
+  }
+
+  const result = await updateContact(
+    id,
+    {
+      ...req.body,
+      photo: photoUrl,
+    },
+    req.user._id,
+  );
+
+  if (!result) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully patched a contact!`,
+    data: result.contact,
+  });
+};
+
+export const deleteContactController = async (req, res, next) => {
   const { id } = req.params;
 
-    const result = await updateContact(id, req.body, req.user._id);
+  const contact = await deleteContact(id, req.user._id);
 
-    if (!result) {
-      return next(createHttpError(404, 'Contact not found'));
-    }
+  if (!contact) {
+    next(createHttpError(404, 'Contact not found'));
+    return;
+  }
 
-    res.json({
-      status: 200,
-      message: `Successfully patched a contact!`,
-      data: result.contact,
-    });
-
-};
-
-// Видалення контакту
-export const deleteContactController = async (req, res, next) => {
-
-    const { id } = req.params;
-
-
-    const contact = await deleteContact(id, req.user._id);
-
-    if (!contact) {
-      return next(createHttpError(404, 'Contact not found'));
-    }
-
-    res.status(204).send();
-
+  res.status(204).send();
 };
